b108b8b5cef69207aa5e2ceb3b04471c96c8b883
Felix Schmitz <felixschmitz57@gmail.com>
2025-01-21 10:35:32 +0100

    Update to config.

--- src/econ_spec_jel/__init__.py
+++ src/econ_spec_jel/__init__.py
@@ -1,4 +1,4 @@
-"""Contains the main namespace of econ_spec_jel."""
+"""Contains the main namespace of econ-spec-jel."""
 
 # Import the version from _version.py which is dynamically created by setuptools-scm
 # when the project is installed with ``pip install -e .``. Do not put it into version
--- src/econ_spec_jel/config.py
+++ src/econ_spec_jel/config.py
@@ -2,9 +2,17 @@
 
 from pathlib import Path
 
+from pytask import DataCatalog
 
 SRC = Path(__file__).parent.resolve()
-BLD = SRC.joinpath("..", "..", "bld").resolve()
+ROOT = SRC.joinpath("..", "..").resolve()
+BLD = ROOT.joinpath("bld").resolve()
+DATA = ROOT.joinpath("data").resolve()
+DOCUMENTS = ROOT.joinpath("documents").resolve()
+
+DATASOURCES = ["constellate", "semantic_scholar"]
+
+DATACATALOG = {datasource: DataCatalog(name=datasource) for datasource in DATASOURCES}
 
 
-__all__ = ["BLD", "SRC"]
+__all__ = ["BLD", "DATA", "DATACATALOG", "DATASOURCES", "DOCUMENTS", "ROOT", "SRC"]
