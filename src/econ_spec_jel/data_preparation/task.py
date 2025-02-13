450a04f4e57341897476fd271755d8b324a42d11
Felix Schmitz <felixschmitz57@gmail.com>
2025-02-13 18:34:15 +0100

    Merge metadata.

---
+++ src/econ_spec_jel/data_preparation/task.py
@@ -0,0 +1,35 @@
+"""Data preparation tasks."""
+
+from pathlib import Path
+from typing import Annotated
+
+import pandas as pd
+
+from econ_spec_jel.config import DATACATALOGS
+
+
+def _merge_metadata(
+    data_catalog: Annotated[Path, DATACATALOGS["metadata"]],
+) -> pd.DataFrame:
+    data = [
+        DATACATALOGS["metadata"][entry].load()
+        for entry in data_catalog._entries
+        if entry != "merged"
+    ]  # SLF001
+    merged_data = pd.DataFrame.from_records(data).sort_values(by="dp_number")
+    return merged_data.reset_index(drop=True)
+
+
+def task_merge_metadata(
+    data_catalog: Annotated[Path, DATACATALOGS["metadata"]],
+) -> Annotated[Path, DATACATALOGS["metadata"]["merged"]]:
+    """Merge metadata of all discussion papers.
+
+    Args:
+        data_catalog: DataCatalog with metadata of all discussion papers.
+
+    Returns
+    -------
+        pd.DataFrame: Merged metadata.
+    """
+    return _merge_metadata(data_catalog=data_catalog)
