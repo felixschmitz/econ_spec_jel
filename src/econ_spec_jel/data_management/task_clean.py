43b179e8b2e8ef9c53117594d425a81215b908cd
Felix Schmitz <felixschmitz57@gmail.com>
2025-02-26 23:28:50 +0100

    Allow for loading the merged data from bld.

--- src/econ_spec_jel/data_management/task_clean.py
+++ src/econ_spec_jel/data_management/task_clean.py
@@ -15,34 +15,30 @@ from econ_spec_jel.data_management.clean_helper import (
 
 
 def task_data_cleaning(
-    merged_metadata: Annotated[Path, DATACATALOGS["data"]["merged"]],
+    merged_data: Annotated[Path, DATACATALOGS["data"]["merged"]],
 ) -> Annotated[Path, DATACATALOGS["data"]["cleaned"]]:
-    """Clean the metadata.
+    """Clean the data.
 
     Args:
-        merged_metadata (pd.DataFrame): Merged metadata.
+        merged_data (pd.DataFrame): Merged data.
 
     Returns
     -------
-        pd.DataFrame: Cleaned metadata.
+        pd.DataFrame: Cleaned data.
     """
-    metadata_complete_title_file_url = merged_metadata.dropna(
-        subset=["title", "file_url"]
-    )
-    return _clean_metadata(metadata_complete_title_file_url)
+    data_complete_title_file_url = merged_data.dropna(subset=["title", "file_url"])
+    return _clean_data(data_complete_title_file_url)
 
 
-def _clean_metadata(
-    merged_metadata: pd.DataFrame,
+def _clean_data(
+    merged_data: pd.DataFrame,
 ) -> pd.DataFrame:
     cleaned_data = pd.DataFrame()
-    cleaned_data["dp_number"] = merged_metadata["dp_number"].astype("uint16[pyarrow]")
-    cleaned_data["title"] = merged_metadata["title"].astype("string[pyarrow]")
-    cleaned_data["author_names"] = merged_metadata["author_names"]
-    cleaned_data["author_urls"] = merged_metadata["author_urls"]
-    cleaned_data["published_raw"] = merged_metadata["published"].astype(
-        "string[pyarrow]"
-    )
+    cleaned_data["dp_number"] = merged_data["dp_number"].astype("uint16[pyarrow]")
+    cleaned_data["title"] = merged_data["title"].astype("string[pyarrow]")
+    cleaned_data["author_names"] = merged_data["author_names"]
+    cleaned_data["author_urls"] = merged_data["author_urls"]
+    cleaned_data["published_raw"] = merged_data["published"].astype("string[pyarrow]")
 
     (
         cleaned_data["published"],
@@ -52,11 +48,11 @@ def _clean_metadata(
     ) = clean_publication_information(cleaned_data["published_raw"])
 
     cleaned_data["publication_year_month"] = create_publication_year_month(
-        merged_metadata["publication_date_year"],
-        merged_metadata["publication_date_month"],
+        merged_data["publication_date_year"],
+        merged_data["publication_date_month"],
     )
-    cleaned_data["abstract"] = merged_metadata["abstract"].astype("string[pyarrow]")
-    cleaned_data["keywords"] = merged_metadata["keywords"]
-    cleaned_data["jel_codes"] = clean_jel_codes(merged_metadata["jel_codes"])
-    cleaned_data["file_url"] = merged_metadata["file_url"].astype("string[pyarrow]")
+    cleaned_data["abstract"] = merged_data["abstract"].astype("string[pyarrow]")
+    cleaned_data["keywords"] = merged_data["keywords"]
+    cleaned_data["jel_codes"] = clean_jel_codes(merged_data["jel_codes"])
+    cleaned_data["file_url"] = merged_data["file_url"].astype("string[pyarrow]")
     return drop_dp_with_missing_data(cleaned_data)
--- src/econ_spec_jel/data_management/task_merge.py
+++ src/econ_spec_jel/data_management/task_merge.py
@@ -6,25 +6,56 @@ from typing import Annotated
 import pandas as pd
 import pytask
 
-from econ_spec_jel.config import DATACATALOGS
+from econ_spec_jel.config import BLD, DATACATALOGS
 
 
-def task_merge_metadata(
-    data_catalog: Annotated[Path, DATACATALOGS["raw"]["metadata"]],
-) -> Annotated[Path, DATACATALOGS["data"]["merged"]]:
-    """Merge metadata of all discussion papers.
+if Path(BLD / "data" / "merged_data.pkl").exists():
+
+    def task_load_merged_data_from_bld(
+        merged_data_path: Annotated[Path, BLD / "data" / "merged_data.pkl"],
+    ) -> Annotated[Path, DATACATALOGS["data"]["merged"]]:
+        """Load merged data from the BLD folder.
+
+        Args:
+            merged_data_path (pd.DataFrame): Merged data.
+
+        Returns
+        -------
+            pd.DataFrame: Merged data.
+        """
+        return pd.read_pickle(merged_data_path)
+
+else:
+
+    def task_merge_data(
+        data_catalog: Annotated[Path, DATACATALOGS["raw"]["data"]],
+    ) -> Annotated[Path, DATACATALOGS["data"]["merged"]]:
+        """Merge data of all discussion papers.
+
+        Args:
+            data_catalog (pytask.DataCatalog): DataCatalog containing data.
+
+        Returns
+        -------
+            pd.DataFrame: Merged data.
+        """
+        return _merge_data(data_catalog=data_catalog)
+
+
+@pytask.mark.skip()
+def task_write_merged_data_to_bld(
+    merged_data: Annotated[Path, DATACATALOGS["data"]["merged"]],
+    out_path: Path = BLD / "data" / "merged_data.pkl",
+) -> None:
+    """Write merged data to the BLD folder.
 
     Args:
-        data_catalog (pytask.DataCatalog): DataCatalog containing metadata.
-
-    Returns
-    -------
-        pd.DataFrame: Merged metadata.
+        merged_data (pd.DataFrame): Merged data.
     """
-    return _merge_metadata(data_catalog=data_catalog)
+    merged_data.to_pickle(out_path)
 
 
-def _merge_metadata(
+def _merge_data(
     data_catalog: pytask.DataCatalog,
 ) -> pd.DataFrame:
     data = [
